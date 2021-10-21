f0269c22b31a9b93d3cccee18fe0ad7a296e9bf4
Mike Coon <mac718@gmail.com>
2021-10-21 09:19:39 -0700

    update getNames controller function

--- controllers/names.js
+++ controllers/names.js
@@ -1,35 +1,17 @@
+const asyncWrapper = require("../middleware/async");
 const Name = require("../models/name");
 const User = require("../models/user");
+const Rating = require("../models/rating");
 
-const getNames = (req, res) => {
+const getNames = asyncWrapper(async (req, res) => {
   let email = req.cookies.user;
-  Name.find((err, names) => {
-    if (err) {
-      console.log(err);
-    }
+  let user = await User.findOne({ name: email });
+  let names = await Name.find({});
+  let userRatings = await Rating.find({ user: user._id });
+  let ratedNames = userRatings.map((rating) => rating.name);
+  let unratedNames = names.filter((name) => !ratedNames.includes(name.name));
 
-    User.find({ name: email }, (err, user) => {
-      if (err) {
-        alert(err);
-      }
-
-      let ratedNames = [];
-
-      if (user.ratings) {
-        ratedNames = user[0].ratings.map((rating) => {
-          return rating[0];
-        });
-      }
-
-      names = names.filter((name) => {
-        return !ratedNames.includes(name.name);
-      });
-      let nameArr = names.map((name) => name.name);
-      console.log(ratedNames, nameArr);
-    });
-
-    return res.json({ names });
-  });
-};
+  res.status(200).json({ names: unratedNames });
+});
 
 module.exports = { getNames };
