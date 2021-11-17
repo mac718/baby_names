74990880c370873ea6845315189d06864fb0abc7
Mike Coon <mac718@gmail.com>
2021-11-16 19:56:29 -0800

    take out redundant code in server

--- server.js
+++ server.js
@@ -41,9 +41,9 @@ app.use(`${baseUrl}emails`, emails);
 
 app.use(errorHandlerMiddlerware);
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-}
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static("client/build"));
+// }
 
 app.get("*", function (request, response) {
   response.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
