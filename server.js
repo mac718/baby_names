d0911452e00c4eafb0c5a295e6624e4b9f6fc8c7
Mike Coon <mac718@gmail.com>
2021-11-16 20:01:51 -0800

    modify catch all route in server

--- server.js
+++ server.js
@@ -41,12 +41,12 @@ app.use(`${baseUrl}emails`, emails);
 
 app.use(errorHandlerMiddlerware);
 
-// if (process.env.NODE_ENV === "production") {
-//   app.use(express.static("client/build"));
-// }
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
 
-app.get("*", function (request, response) {
-  response.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
+app.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
 const port = process.env.PORT || 3001;
