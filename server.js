774c9f0a958d2e2462040df398ab4d3550675f2f
Mike Coon <mac718@gmail.com>
2021-11-16 20:08:01 -0800

    change cors origin to heroku url

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use((req, res, next) => {
 
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "https://rocky-temple-34078.herokuapp.com/",
     credentials: true,
   })
 );
